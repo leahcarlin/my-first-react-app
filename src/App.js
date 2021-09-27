d64d28ad31b590694eea3c04f82120cc92588a14
leahcarlin <leahrcarlin@gmail.com>
2021-09-27 11:15:20 +0200

    Removed logo, fixed warning

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Button from "./components/Button"
 import Title from "./components/Title"
+
+
+
 function App() {
-
-
 
   return (
     <div className="App">
